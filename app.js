1b4d5860e0e8ce66a274f4b8e30fe93444071d3f
Julian Bull <julianbullmagic@gmail.com>
2023-08-14 21:12:30 +1000

    fff

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const { createServer } = require("http");
-
 const express = require("express");
 const { Server } = require("socket.io");
-
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
-
 const port = process.env.PORT || 3000;
-
 app.use(express.static("public"));
 
 let activeUsers = [];
 let robot=""
 io.on("connection", (socket) => {
+  console.log(socket.id,"connection")
     socket.on("frame", (data) => {
         io.emit("frame",Buffer.from(data, 'base64').toString());
     });
