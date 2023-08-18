50334d760b303e41d8d71ff8159be726e533be95
Julian Bull <julianbullmagic@gmail.com>
2023-08-18 20:28:42 +1000

    ffff

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ const socket = io("/");
 let detectingObjects=false
 
 socket.on("frame", (data) => {
+  console.log(data)
 let img=document.getElementById('robotcam')
 img.src = `data:image/jpeg;base64,${data}`
 });
