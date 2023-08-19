4d1d4b5c1f7b6ef800d2e252999211eb35d3284d
Julian Bull <julianbullmagic@gmail.com>
2023-08-19 10:34:23 +1000

    ffff

--- public/js/index.js
+++ public/js/index.js
@@ -1,30 +1,38 @@
 import { HfInference } from 'https://cdn.jsdelivr.net/npm/@huggingface/inference@2.6.1/+esm';
-let HF_ACCESS_TOKEN="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"
+
+let HF_ACCESS_TOKEN = "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";
 const inference = new HfInference(HF_ACCESS_TOKEN);
 
-async function getCaption(base64Image){
-  // Decode the base64 string into binary data
+// Convert base64 image string to caption
+async function getCaption(base64Image) {
   const binaryData = atob(base64Image);
-  // Convert the binary data to a Uint8Array
   const byteArray = new Uint8Array(binaryData.length);
   for (let i = 0; i < binaryData.length; i++) {
     byteArray[i] = binaryData.charCodeAt(i);
   }
-  const imageBlob = new Blob([byteArray.buffer], { type: 'image/jpeg' }); // Adjust the type if needed
-  let text = await inference.imageToText({
+  const imageBlob = new Blob([byteArray.buffer], { type: 'image/jpeg' });
+
+  const text = await inference.imageToText({
     data: imageBlob,
     model: 'Salesforce/blip-image-captioning-base',
   });
-  console.log(text)
-  return text
+
+  return text;
 }
 
 const socket = io("/");
-let detectingObjects=false
+let captionPromise = Promise.resolve(); // Initialize with a resolved promise
 
-socket.on("frame", (data) => {
-let caption=getCaption(data)
-console.log(caption)
-let img=document.getElementById('robotcam')
-img.src = `data:image/jpeg;base64,${data}`
+socket.on("frame", async (data) => {
+  // Wait for the previous caption processing to complete before starting the next one
+  await captionPromise;
+
+  // Process the caption
+  captionPromise = (async () => {
+    const caption = await getCaption(data);
+    console.log(caption);
+
+    let img = document.getElementById('robotcam');
+    img.src = `data:image/jpeg;base64,${data}`;
+  })();
 });
